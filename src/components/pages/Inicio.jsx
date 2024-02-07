899a056369675feb5cbb2540d95e37ff15dd850f
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-07 20:36:39 -0300

    se agregaron las cards de productos al componente inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,5 +1,9 @@
 import banner from "../../assets/banner3.png";
 import { Row, Col, Card, Button, Container } from "react-bootstrap";
+import producto1 from "../../assets/cafe_americano.png";
+import producto2 from "../../assets/capuchino.png";
+import producto3 from "../../assets/croissant_chocolate.png";
+import producto4 from "../../assets/te_verde.png";
 
 const Inicio = () => {
   return (
@@ -7,61 +11,62 @@ const Inicio = () => {
       <img src={banner} alt="" className="w-100 img-fluid" />
       
       <Container>
-      <h1 className="mt-4 display-2">Nuestros productos</h1>
-      <Row className="d-flex justify-content-center">
+      <h1 className="mt-5 display-2">Nuestros productos</h1>
+      <Row className="d-flex justify-content-center mt-4">
         <Col md={4} className="mb-2" >
           <Card >
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={producto1} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              <Card.Title>Cafe americano</Card.Title>
+              <Card.Text className="lead">
+                Descripcion: Una taza de cafe suave y aromatico.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <p className="h5"><strong>Precio: $250</strong></p>
+              
             </Card.Body>
+            <Card.Footer className="text-muted text-end"><Button variant="success">Ver más</Button></Card.Footer>
           </Card>
         </Col>
 
         <Col md={4} className="mb-2" >
           <Card >
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={producto2} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              <Card.Title>Capuchino</Card.Title>
+              <Card.Text className="lead">
+                Descripcion: Espuma de leche cremosa sobre un espresso fuerte.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <p className="h5"><strong>Precio: $250</strong></p>
             </Card.Body>
+            <Card.Footer className="text-muted text-end"><Button variant="success">Ver más</Button></Card.Footer>
           </Card>
         </Col>
 
         <Col md={4} className="mb-2" >
           <Card >
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={producto3} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              <Card.Title>Croissant de chocolate</Card.Title>
+              <Card.Text className="lead">
+                Descripcion: Delicioso croissant relleno de suave chocolate.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <p className="h5"><strong>Precio: $250</strong></p>
             </Card.Body>
+            <Card.Footer className="text-muted text-end"><Button variant="success">Ver más</Button></Card.Footer>
           </Card>
         </Col>
 
         <Col md={4} className="mb-2" >
           <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={producto4} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              <Card.Title>Té verde</Card.Title>
+              <Card.Text className="lead">
+                Descripcion: Té verde organico con un sabor suave y revitalizante.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <p className="h5"><strong>Precio: $250</strong></p>
             </Card.Body>
+            <Card.Footer className="text-muted text-end"><Button variant="success">Ver más</Button></Card.Footer>
           </Card>
         </Col>
       </Row>
