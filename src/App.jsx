ef829cb48f31d3a99d31af7f024a7aea7c61a731
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-07 18:33:53 -0300

    se creo el navbar del componente Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Menu from "./components/common/Menu";
+import Footer from "./components/common/Footer";
+import Inicio from "./components/pages/Inicio";
+import Administrador from "./components/pages/Administrador";
+import Error404 from "./components/pages/Error404";
 
 function App() {
- 
-
   return (
     <>
-      
+      <Menu></Menu>
+      {/*<Inicio></Inicio>*/}
+      {/*<Administrador></Administrador>*/}
+      <Error404></Error404>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,10 +1,25 @@
+import {Nav, Navbar, Container} from "react-bootstrap";
+import logo from '../../assets/Coffee_Logo.png';
 
 
 const Menu = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img src={logo} alt="logo rollingCoffe" className="img-fluid" width={200}/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="">Inicio</Nav.Link>
+            <Nav.Link href="">Administrador</Nav.Link>
+            <Nav.Link href="">Registro</Nav.Link>
+            <Nav.Link href="">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
