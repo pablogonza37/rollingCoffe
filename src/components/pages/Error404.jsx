b0376889c39aad66245e470c04582878b5d7216d
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-07 20:57:29 -0300

    se creo el componente Error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import error404 from '../../assets/error404.png'
+import Button from 'react-bootstrap/Button';
 
 const Error404 = () => {
     return (
-        <div>
-            
+        <section>
+        <div className='text-center'>
+           <img src={error404} alt="imagen error 404" className='w-50'/> 
         </div>
+        <div className='d-flex justify-content-center mb-5'>
+        <Button variant="primary">Volver al inicio</Button>
+        </div>
+        </section>
     );
 };
 
