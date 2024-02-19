c3b03d06ae091c0fade4b17ead7a8f6c81e95f09
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-18 22:15:36 -0300

    se agregaron los las rutas a los menus

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,23 @@ import Footer from "./components/common/Footer";
 import Inicio from "./components/pages/Inicio";
 import Administrador from "./components/pages/Administrador";
 import Error404 from "./components/pages/Error404";
+import FormularioProducto from "./components/pages/producto/FormularioProducto";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <Menu></Menu>
-      {/*<Inicio></Inicio>*/}
-      <Administrador></Administrador>
-      {/*<Error404></Error404>*/}
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/administrador"
+          element={<Administrador></Administrador>}
+        ></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
       <Footer></Footer>
+    </BrowserRouter>
     </>
   );
 }
