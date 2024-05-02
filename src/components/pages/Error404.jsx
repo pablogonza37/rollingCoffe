24e944b293beea149874c4ba70db603d014da3e8
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-05-02 20:54:30 -0300

    se agrego ruta al boton de el componete 404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,14 +1,15 @@
 import error404 from '../../assets/error404.png'
 import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
     return (
-        <section className="mainSection text-center">
+        <section className="mainSection text-center mt-3">
         <div className='text-center'>
-           <img src={error404} alt="imagen error 404" className='w-25'/> 
+           <img src={error404} alt="imagen error 404" className='img-fluid'/> 
         </div>
         <div className='d-flex justify-content-center mb-5'>
-        <Button variant="primary">Volver al inicio</Button>
+        <Button variant="success" as={Link} to='/'>Volver al inicio</Button>
         </div>
         </section>
     );
